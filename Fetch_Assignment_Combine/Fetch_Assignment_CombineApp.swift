0e27c51de4365bf9ce1cf84c889d69cd6303e584
rohit-iwnl <rohitmanivel9@gmail.com>
2024-03-14 21:28:35 -0700

    Initial Commit

---
+++ Fetch_Assignment_Combine/Fetch_Assignment_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Assignment_CombineApp.swift
+//  Fetch_Assignment_Combine
+//
+//  Created by Rohit Manivel on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Assignment_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
